9b2886f74762f11d94d1cdfc11f926be0c45bd6a
Sergey Obmolov <winchestersoro@gmail.com>
2025-01-12 23:44:32 +0300

    first slide

---
+++ src/components/FirstSlide/LendingCyberpunk.tsx
@@ -0,0 +1,13 @@
+
+import styles from './LendingCyberpunk.module.css';
+import Protoganist from '../../assets/Protoganist.jpeg';
+const LendingCyberpunk: React.FC = () => {
+    return (
+        <div className={styles.LendingPage} style={{ backgroundImage: `url(${Protoganist})` }}>
+            
+            
+        </div>
+    );
+};
+
+export default LendingCyberpunk;
---
+++ src/components/SecondSlide/SecondSlide.tsx
@@ -0,0 +1,11 @@
+
+import styles from './SecondSlide.module.css';
+const SecondSlide = () => {
+    return (
+        <div className={styles.SecondSlide}>
+            
+        </div>
+    );
+};
+
+export default SecondSlide;
