0d6851c1279e0a7e96e053b46ee00e9fd94c4105
Sergey Obmolov <winchestersoro@gmail.com>
2025-02-01 05:27:41 +0300

    the fourth slide

--- src/components/ThirdSlide/ThirdSlide.tsx
+++ src/components/ThirdSlide/ThirdSlide.tsx
@@ -3,6 +3,7 @@ import InputForm from '../UI/InputForm/InputForm';
 import Photo from '../UI/FormPhoto/Photo';
 import PhotoForm from '../../assets/photo/PhotoForm.jpeg';
 
+
 const ThirdSlide = () => {
 
    
@@ -12,23 +13,36 @@ const ThirdSlide = () => {
 
             <div className={styles.formSection}>
 
-                <div className={styles.description}>
-                    <h1 className={styles.cyberH}>Cyberpunk 2077</h1>
-                    <p className={styles.cyberP}>Подпишитесь на официальную новостную рассылку Cyberpunk 2077.</p>
-                    <p className={styles.cyberP}>Будьте в курсе всех новостей вселенной</p>
+                <div className={styles.descBlock}>
+
+                    <div className={styles.description}>
+                        <h1 className={styles.cyberH}>Cyberpunk 2077</h1>
+                        <p className={styles.cyberP}>Подпишитесь на официальную новостную рассылку Cyberpunk 2077.</p>
+                        <p className={styles.cyberP}>Будьте в курсе всех новостей вселенной</p>
+                    </div>
+
+                    <a href="https://www.cdprojektred.com/en" className={styles.photoDownLink}>
+                        <div className={styles.cdPr}>
+                        <a href="https://www.cdprojektred.com/en" className={styles.photoDownLink}> </a>
+
+                        </div>
+                    </a>
+
                 </div>
+                
                 
                 <div className={styles.formBlock}>
                     <InputForm />
-                    <Photo size="standard" src={PhotoForm} alt="ФотоФорм" > {" "} </Photo>
+                    <Photo size="large" src={PhotoForm} alt="ФотоФорм" > {" "} </Photo>
                 </div>
 
             </div>
 
-            <div className={styles.photoDown}>
+                <div className={styles.photoDown}>
 
-            </div>
-
+                </div>
+            
+        
         </div>
     );
 };
